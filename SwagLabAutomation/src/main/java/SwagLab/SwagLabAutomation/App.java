1614c2c78b011e6ffdcda6c8d02b8d0c2d1dbc0e
UnnatiRaval <Unnati.raval1990@gmail.com>
2024-07-28 23:45:57 +0530

    add selenium framework project

---
+++ SwagLabAutomation/src/main/java/SwagLab/SwagLabAutomation/App.java
@@ -0,0 +1,13 @@
+package SwagLab.SwagLabAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
