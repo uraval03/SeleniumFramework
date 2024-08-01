1711477d3e4a7d5ebe9b6048fb23eea29562b227
UnnatiRaval <Unnati.raval1990@gmail.com>
2024-08-01 23:12:00 +0530

    add java program

---
+++ src/main/java/utils/ElementFetch.java
@@ -0,0 +1,21 @@
+package utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import base.BaseTest;
+
+public class ElementFetch {
+	
+	public WebElement getWebElement(String identifierType, String identifireValue})
+	{
+		switch(identifierType) {
+		
+		class "XPATH" :
+		return BaseTest.driver.findElement(By.cssSelector(null));
+		
+	daful:
+		retrun null;
+		}
+
+}
